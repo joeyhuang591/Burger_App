057626a7a225a52d3d804b78d346c970765bc91e
Joey Huang <j.huang@neopost.com>
2018-05-23 14:23:13 -0400

    test

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -18,6 +18,7 @@ const orderSummary = (props) => {
             <p>Continue to checkout?</p>
             <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
             <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+
         </div>
     );
 };

23e303070fe411e5f1f8369b9b3a5cd21c0c9247
Joey Huang <j.huang@neopost.com>
2018-05-29 14:49:06 -0400

    add responsive design

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 // import Aux from '../../hoc/Aux';
 
@@ -6,14 +6,32 @@ import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <div>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </div>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
+    sideDrawerClosedHandler = () => {
+        this.setState({ showSideDrawer: false })
+    }
+    sideDrawerToggleHandler = () => {
+        //set state is asyn, so need to use prevState to ensure the correction
+        this.setState((prevState) => {
+            return { showSideDrawer: !prevState.showSideDrawer };
+        });
+    }
+    render() {
+        return (
+            <div>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </div>
+        )
 
-export default layout;
+    }
+}
+
+
+export default Layout;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Modal.css';
 import Backdrop from '../Backdrop/Backdrop';
-const modal = (props) => (
-    <div>
-        <Backdrop show={props.show} clicked={props.modalClosed} />
-        <div className={classes.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
-            }} >
-            {props.children}
-        </div>
-    </div>
+
+class Modal extends Component {
+    //Optimization 
+    //Don't update the state or props if the conditional statement is false to
+    //ensure not to render
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    }
+
+    // componentWillUpdate() {
+    //     console.log('[Modal] will update');
+    // }
+
+    render() {
+        return (
+            <div>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+                <div className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }} >
+                    {this.props.children}
+                </div>
+            </div>
+        );
+    }
+}
 
 
-);
-
-export default modal;
+export default Modal;

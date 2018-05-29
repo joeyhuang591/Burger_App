c7fe94b47a55da1f07316fa8d9fce806eb8f0ebe
Joey Huang <j.huang@neopost.com>
2018-05-29 16:05:24 -0400

    add axios for http request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,7 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
-
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -82,7 +82,23 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You continue')
+        //alert('You continue')
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Joey',
+                address: {
+                    street: 'Test 1',
+                    zip: '11111',
+                    country: 'US'
+                },
+                email: 'test@test.com'
+            },
+            deliveryMethod: 'fasttest'
+        }
+        axios.post('/orders.json', order).then(response => console.log(response))
+            .catch(error => console.log(error));
     }
 
     render() {
